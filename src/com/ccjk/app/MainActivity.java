dd7f02c89e5cf52b05d9b05602502976606fd6b6
tianyong2 <tianyong428@163.com>
2014-12-27 13:59:50 +0800

    start edit

--- src/com/ccjk/app/MainActivity.java
+++ src/com/ccjk/app/MainActivity.java
@@ -2,12 +2,18 @@ package com.ccjk.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private TextView tv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		tv=(TextView)findViewById(R.id.tv_test);
+		tv.setText("sl;dkfj");
 	}
+	
 }
